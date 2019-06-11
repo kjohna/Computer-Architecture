7963abdd502fe856a9112968bfe9715b538cfc2c
kjohna <43828763+kjohna@users.noreply.github.com>
2019-06-11 13:31:05 -0600

    basic setup, HLT working

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,18 @@
 #include "cpu.h"
 
 #define DATA_LEN 6
+#define HLT 0b00000001 // 01
+
+// helpers to read and write cpu's ram
+unsigned int cpu_ram_read(struct cpu *cpu, int index)
+{
+  return cpu->ram[index];
+};
+
+void cpu_ram_write(struct cpu *cpu, int index, unsigned char value)
+{
+  cpu->ram[index] = value;
+};
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -8,18 +20,19 @@
 void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+      // From print8.ls8
+      0b10000010, // LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, // PRN R0
+      0b00000000,
+      0b00000001 // HLT
   };
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
+  for (int i = 0; i < DATA_LEN; i++)
+  {
     cpu->ram[address++] = data[i];
   }
 
@@ -31,10 +44,11 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    // TODO
+    break;
 
     // TODO: implement more ALU ops
   }
@@ -46,15 +60,40 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
+  int oper_count, operands[2];
+  unsigned char instruction;
 
-  while (running) {
+  while (running)
+  {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    instruction = cpu_ram_read(cpu, cpu->pc);
     // 2. Figure out how many operands this next instruction requires
+    oper_count = instruction >> 6;
     // 3. Get the appropriate value(s) of the operands following this instruction
+    for (int i = 0; i < oper_count; i++)
+    {
+      operands[i] = cpu_ram_read(cpu, cpu->pc + i + 1);
+    }
+    // what's it up to?
+    printf("instr: %02x\n");
+    printf("pc: %d\n", cpu->pc);
+    printf("oper_count: %d\n", cpu->pc);
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
+    switch (instruction)
+    {
+    case HLT:
+      printf("HLT command received. Exiting.\n");
+      running = 0;
+      break;
+
+    default:
+      printf("Unknown command: %02x\n", instruction);
+      break;
+    }
     // 6. Move the PC to the next instruction.
+    cpu->pc = cpu->pc + 1 + oper_count;
   }
 }
 
@@ -64,4 +103,9 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  cpu->pc = 0;
+  cpu->ir = 0;
+  cpu->mar = 0;
+  cpu->mdr = 0;
+  cpu->fl = 0;
 }
